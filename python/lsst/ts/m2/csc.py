993ef8568fa8ebf0671b9c6014281a9b95e013ba
Tiago Ribeiro <tiago.astro@gmail.com>
2020-03-02 13:24:34 -0700

    Modernize CSC and tests.

--- python/lsst/ts/m2/csc.py
+++ python/lsst/ts/m2/csc.py
@@ -8,37 +8,40 @@ class M2(salobj.ConfigurableCsc):
     """
 
     def __init__(self, config_dir=None, initial_state=salobj.State.STANDBY,
-                 initial_simulation_mode=0):
+                 simulation_mode=0):
         schema_path = pathlib.Path(__file__).resolve().parents[4].joinpath("schema", "m2.yaml")
         super().__init__("MTM2", index=0, schema_path=schema_path, config_dir=config_dir,
                          initial_state=initial_state,
-                         initial_simulation_mode=initial_simulation_mode)
+                         simulation_mode=simulation_mode)
 
         self.config = None
 
     async def do_applyBendingMode(self, id_data):
         """Apply bending mode.
         """
-        pass
+        self.assert_enabled()
 
     async def do_applyForce(self, id_data):
         """Apply force.
         """
-        self.config.max_force
+        self.assert_enabled()
         pass
 
     async def do_setCorrectionMode(self, id_data):
         """Set correction Mode.
         """
+        self.assert_enabled()
         pass
 
     async def do_positionMirror(self, id_data):
         """Position Mirror.
         """
+        self.assert_enabled()
         pass
 
     async def do_moveAxialActuator(self, id_data):
         """Move axial actuator."""
+        self.assert_enabled()
         pass
 
     @staticmethod
--- tests/test_csc.py
+++ tests/test_csc.py
@@ -1,7 +1,7 @@
 
-import unittest
 import pathlib
-import asyncio
+import unittest
+import asynctest
 
 from lsst.ts import salobj
 from lsst.ts.m2 import M2
@@ -9,45 +9,30 @@ from lsst.ts.m2 import M2
 TEST_CONFIG_DIR = pathlib.Path(__file__).resolve().parent.joinpath("data", "config")
 
 
-class Harness:
-    def __init__(self, initial_state=salobj.State.STANDBY,
-                 config_dir=None,
-                 initial_simulation_mode=0):
+class TestM2CSC(salobj.BaseCscTestCase, asynctest.TestCase):
 
-        salobj.test_utils.set_random_lsst_dds_domain()
+    def basic_make_csc(self, initial_state, config_dir, simulation_mode):
+        return M2(
+            initial_state=initial_state,
+            config_dir=config_dir,
+            simulation_mode=simulation_mode
+        )
 
-        self.csc = M2(config_dir=config_dir,
-                      initial_state=initial_state,
-                      initial_simulation_mode=initial_simulation_mode)
-
-        self.remote = salobj.Remote(self.csc.domain, "MTM2")
-
-    async def __aenter__(self):
-        await asyncio.gather(self.csc.start_task,
-                             self.remote.start_task)
-        return self
-
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
-        await asyncio.gather(self.csc.close(),
-                             self.remote.close())
-
-
-class TestM2CSC(unittest.TestCase):
-
-    def test_basic_state_transition(self):
-
-        async def doit():
-            async with Harness(config_dir=TEST_CONFIG_DIR) as harness:
-
-                evt_timeout = 5.
-
-                state = await harness.remote.evt_summaryState.next(flush=False,
-                                                                   timeout=evt_timeout)
-
-                self.assertEqual(salobj.State(state.summaryState),
-                                 salobj.State.STANDBY)
-
-        asyncio.get_event_loop().run_until_complete(doit())
+    async def test_standard_state_transitions(self):
+        async with self.make_csc(
+                initial_state=salobj.State.STANDBY,
+                config_dir=None,
+                simulation_mode=0
+        ):
+            await self.check_standard_state_transitions(
+                enabled_commands=(
+                    "applyBendingMode",
+                    "applyForce",
+                    "setCorrectionMode",
+                    "positionMirror",
+                    "moveAxialActuator",
+                )
+            )
 
 
 if __name__ == "__main__":
