59c9d04f37017cf62cf71d35164c58c229167d1a
Te-Wei Tsai <twtsaia@gmail.com>
2021-06-22 16:11:33 -0700

    Add the config_schema.py.

---
+++ python/lsst/ts/m2/config_schema.py
@@ -0,0 +1,40 @@
+# This file is part of ts_m2.
+#
+# Developed for the Vera Rubin Observatory Telescope and Site Systems.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+__all__ = ["CONFIG_SCHEMA"]
+
+import yaml
+
+CONFIG_SCHEMA = yaml.safe_load(
+    """
+$schema: http://json-schema.org/draft-07/schema#
+$id: https://github.com/lsst-ts/ts_m2/blob/master/schema/m2.yaml
+# title must end with one or more spaces followed by the schema version, which must begin with "v"
+title: M2 v1
+description: Schema for M2 configuration files
+type: object
+properties:
+  lut_path:
+    description: Relative location of the Look-Up Tables, with respect to configuration package.
+    type: string
+    default: "harrisLUT/"
+"""
+)
