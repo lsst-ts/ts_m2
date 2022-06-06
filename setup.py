dc78758f743d2867d90bbb8500248fe6606a8870
Te-Wei Tsai <ttsai@lsst.org>
2022-06-06 10:17:23 -0400

    Update based on Wouter's help.

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
-import pathlib
 from setuptools import setup
 import setuptools_scm
 
-setup(
-    version=setuptools_scm.get_version(),
-    scripts=[str(path) for path in pathlib.Path("bin").glob("*.py")],
-)
+setup(version=setuptools_scm.get_version())
